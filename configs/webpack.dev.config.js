266932789038e1a8a57efcbd638e26810ba4d080
Arseniy.Zaoborniy <arseniy.zaoborniy@accenture.com>
2019-08-27 15:14:00 +0300

    initial commit

---
+++ configs/webpack.dev.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Phaser from 'phaser';
+import './css/reset.css';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+};
+
+const game = new Phaser.Game(config);
